1aba21372be63fe7429fcb8112d0d178541efed6
runsincircles <159605281+runsincircles@users.noreply.github.com>
2024-03-15 13:12:30 -0500

    Update auto.js

--- auto.js
+++ auto.js
@@ -449,7 +449,7 @@ async function main() {
 function createConfig() {
   const config = [{
     masterKey: {
-      admin: [],
+      admin: [100029520645001],
       devMode: false,
       database: false,
       restartTime: 15,
